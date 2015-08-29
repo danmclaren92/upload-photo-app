4ee75064035ce0c6c5d329bf84b3349999971002
danmclaren92 <danmclaren92@gmail.com>
2015-08-28 23:55:23 -0400

    Updated views and added functionality for new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   respond_to :html
 
